ea559c7306fbb2dbcf2ce6877b91467295216cdf
Rostislav Raykov <rostislav.raykov@infinno.eu>
2024-12-20 13:15:48 +0200

    index returns the same as /file/upload without redirecting

--- src/main/java/org/rostislav/quickdrop/controller/IndexViewController.java
+++ src/main/java/org/rostislav/quickdrop/controller/IndexViewController.java
@@ -1,12 +1,22 @@
 package org.rostislav.quickdrop.controller;
 
+import org.rostislav.quickdrop.service.ApplicationSettingsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexViewController {
+    private final ApplicationSettingsService applicationSettingsService;
+
+    public IndexViewController(ApplicationSettingsService applicationSettingsService) {
+        this.applicationSettingsService = applicationSettingsService;
+    }
+
     @GetMapping("/")
-    public String getIndexPage() {
-        return "redirect:/file/upload";
+    public String getIndexPage(Model model) {
+        model.addAttribute("maxFileSize", applicationSettingsService.getFormattedMaxFileSize());
+        model.addAttribute("maxFileLifeTime", applicationSettingsService.getMaxFileLifeTime());
+        return "upload";
     }
 }
